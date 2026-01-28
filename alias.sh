e46293f28693ba1565371444627a8f46a9020a65
fatalus69 <der_fatal_@outlook.com>
2026-01-28 19:29:41 +0100

    fix: unconditional zsh completion loading & add some aliases

--- alias.sh
+++ alias.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
+# Exit if not an interactive shell
+[[ $- != *i* ]] && return
+
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
 alias ls='ls --color=auto'
+alias ll='ls -laFh --color=auto'
 alias grep='grep --color=auto'
 alias python='python3'
 alias onefetch='onefetch --nerd-fonts --include-hidden --no-bots'
 alias drush='ddev drush'
+alias laravel_clear='php artisan optimize &&php artisan cache:clear && php artisan config:clear && php artisan route:clear && php artisan view:clear'
 
 # Source bash/zshrc
-if [[ $SHELL == *"zsh" ]]; then
+if [[ -n "$ZSH_VERSION" ]]; then
     alias sz="source $HOME/.zshrc"
-elif [[ $SHELL == *"bash" ]]; then
+elif [[ -n "$BASH_VERSION" ]]; then
     alias sb="source $HOME/.bashrc"
 fi
 
--- exports.sh
+++ exports.sh
@@ -9,10 +9,14 @@ export PATH=$PATH:/usr/local/go/bin
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$BUN_INSTALL/bin:$PATH"
 
-# Load completions for bun
-[ -f "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+# Load nvm and its completions
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-# Assuming leahneukirchen/lr was installed via make install
-if [[ $SHELL == *"zsh" ]]; then
+if [[ -n "$ZSH_VERSION" ]]; then
+  # Assuming leahneukirchen/lr was installed via make install
   [ -f "/usr/local/share/zsh/site-functions/_lr" ] && source "/usr/local/share/zsh/site-functions/_lr"
+  
+  # Load completions for bun
+  [ -f "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
 fi
