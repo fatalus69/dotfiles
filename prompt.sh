3f53c5605f2573542a1595862cbfdc5f8b73c332
fatalus <der_fatal_@outlook.com>
2026-03-27 13:31:24 +0100

    chore: prompt for server

--- prompt.sh
+++ prompt.sh
@@ -2,3 +2,6 @@
 
 # Just the standard bash prompt...
 PS1='\[\e[92m\]\u@\h\[\e[0m\]:\[\e[34m\]\w\[\e[0m\]\\$ '
+
+# Root prompt for Servers:
+# PS1='\[\e[91m\]\u\[\e[0m\]@\[\e[96m\]\h\[\e[0m\]:\[\e[33m\]\w\[\e[0m\]\$
